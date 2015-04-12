0ca3b78cec985adf895be3a637a37057f4211f21
Giff Song <giff.song@gmail.com>
2015-04-13 00:28:37 +0900

    Add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: ['node_modules/**'],
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        '**/*.js',
+      ]
+    },
+
+  });
+
+  grunt.registerTask('lint', ['jshint']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['lint']);
+};
